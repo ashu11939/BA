5ff72cb78ccdba799e1bdc80dddb8b15d20f4701
Ashutosh Kumar <ashutosh11939@gmail.com>
2018-11-08 16:20:04 +0530

    load Data class added for all basic operations on dataframe

--- BusinessAnalyticsPy/app.py
+++ BusinessAnalyticsPy/app.py
@@ -1,12 +1,40 @@
 from flask import Flask
 from datetime import datetime
 import re
+from loadData import LoadData
 
 app = Flask(__name__)
-
+loadData = LoadData()
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+
+    stockStatementURI = 'C:\\Users\\Ashutosh\\workspace\\BusinessAnalytics\\local\\BusinessAnalyticsPy\\fwdlezarstock\\STOCK-3010.csv'
+    skipRows = 2
+    stockStatement = loadData.loadCsvData(stockStatementURI, skipRows)
+    stockStatementDF = loadData.convertCSVToDataFrame(stockStatement)
+    product = stockStatementDF['Product Name']
+
+    cost = stockStatementDF['Purchase Price'] * \
+        stockStatementDF['Current Stock']
+
+    # Create a new array for X-Axis being the 1st column and Y-axis being the second column
+    data = loadData.convertToJsonObject(["products", "cost"], [product.to_json(), cost.to_json()])
+    product.to_json()
+    # data = '{"products":' + product.to_json() + ',"cost":' + cost.to_json() + "}"
+    stockList = stockStatementDF['Current Stock']
+    maxValue = loadData.maxValueFromlist(stockList)
+    minValue = loadData.minValueFromList(stockList)
+    maxValueId = loadData.maxValueIDFromlist(stockList)
+    minValueId = loadData.minValueIDFromList(stockList)
+    maxRowData = stockStatementDF.loc[(stockStatementDF['Current Stock'] == maxValue)]
+    minRowData = stockStatementDF.loc[(stockStatementDF['Current Stock'] == minValue)]
+    
+    rowData = loadData.getRowFromDataframe(stockStatementDF, maxValueId)
+
+    print(stockStatementDF.loc[maxValueId]) 
+    print(stockStatementDF.loc[minValueId]) 
+    return rowData.to_json()
+
 
 @app.route("/hello/<name>")
 def hello_there(name):
