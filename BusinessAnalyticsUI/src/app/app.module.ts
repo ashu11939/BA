d285ebebf0b399793372d28298d4d301f6b52c30
Ashutosh Kumar <ashutosh11939@gmail.com>
2018-11-09 12:32:13 +0530

    UI Template added/ Basic layout

--- BusinessAnalyticsUI/src/app/app.module.ts
+++ BusinessAnalyticsUI/src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartModule } from 'angular2-highcharts';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 declare var require: any;
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
     AppRoutingModule,
     ChartModule.forRoot(require('highcharts')),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
